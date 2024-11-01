e18866beaa130d0b51b3d5f5edf1a0f5e708771e
Name <some@example.com>
2024-11-01 23:27:01 +0300

    Add test for Board, add separate folder for Board

---
+++ src/components/Board/Board.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Board = () => {
+  const createBoard = () => {
+    const cells = [];
+    "ABCDEFGHIJ".split("").forEach((l) => {
+      for (let i = 1; i <= 10; i++) {
+        cells.push(
+          <div
+            key={`${l}${i}`}
+            data-testid={`${l}${i}`}
+            role="gridcell"
+            className={`cell ${l}${i}`}
+          ></div>
+        );
+      }
+    });
+    return cells;
+  };
+
+  return <div className="board">{createBoard()}</div>;
+};
+
+export default Board;
