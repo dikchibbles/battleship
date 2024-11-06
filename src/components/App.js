af80d12555d813ba4b53eeed5a285627a01c0208
Name <some@example.com>
2024-11-06 20:36:08 +0300

    Change Board design, add Own and Enemy ships board switch

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./Board/Board";
 import Header from "./Header";
 import Dropdown from "./Dropdown/Dropdown";
@@ -6,15 +6,38 @@ import RulesContent from "../content/RulesContent";
 import "./App.css";
 
 const App = () => {
+  const [activeBoard, setActiveBoard] = useState("ownShips");
+
+  const toggleActiveBoard = () => {
+    activeBoard === "ownShips"
+      ? setActiveBoard("enemyShips")
+      : setActiveBoard("ownShips");
+  };
+
   return (
     <>
-      <div className="faq">
-        <Dropdown title="Rules">
-          <RulesContent />
-        </Dropdown>
-      </div>
       <Header />
-      <div className="container">{<Board />}</div>
+      <div className="main">
+        <div className="faq">
+          <Dropdown title="Rules">
+            <RulesContent />
+          </Dropdown>
+        </div>
+        <div className="board-container">
+          <div className="board-options">
+            <label
+              htmlFor="filter"
+              className="switch"
+              aria-label="Toggle Filter"
+            >
+              <input type="checkbox" id="filter" onClick={toggleActiveBoard} />
+              <span>Own</span>
+              <span>Enemy</span>
+            </label>
+          </div>
+          {<Board />}
+        </div>
+      </div>
     </>
   );
 };
--- src/content/RulesContent.js
+++ src/content/RulesContent.js
@@ -4,11 +4,27 @@ const RulesContent = () => {
   return (
     <div>
       <div className="game-components">
-        <h6>Game Components</h6>
+        <h3>Game Components</h3>
         <ul>
-          <strong>Game Boards:</strong> Each player has a primary ocean grid
-          (10x10) for positioning their fleet and a secondary targeting grid to
-          track shots fired.
+          <li>
+            <strong>Game Boards:</strong> Each player has a primary ocean grid
+            (10x10) for positioning their fleet and a secondary targeting grid
+            to track shots fired.
+          </li>
+          <li>
+            <strong>Ships:</strong> Each player has a fleet of five ships:
+            <ul>
+              <li>Carrier (5 spaces)</li>
+              <li>Battleship (4 spaces)</li>
+              <li>Cruiser (3 spaces)</li>
+              <li>Submarine (3 spaces)</li>
+              <li>Destroyer (2 spaces)</li>
+            </ul>
+          </li>
+          <li>
+            <strong>Peg Markers:</strong> White pegs to mark misses, and red
+            pegs to mark hits.
+          </li>
         </ul>
       </div>
     </div>
